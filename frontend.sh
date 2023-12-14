4d5e434d6ff70388fc2f500b09c97caa27858240
yamunasreeoggu <yamunasree321@gmail.com>
2023-12-14 14:54:26 -0500

    Practice | Yamuna |corrected code for unzip

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,18 @@
 echo Install Nginx
 dnf install nginx -y
 
+echo Copy Expense Conf file
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 echo Delete Nginx default content
 rm -rf /usr/share/nginx/html/*
 
 echo Download and Extract code
 curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-echo Copy Expense Conf file
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo Unzip files
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 echo restart Nginx Service
 systemctl enable nginx
