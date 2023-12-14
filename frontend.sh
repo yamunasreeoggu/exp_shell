625ed18685f858dcd2091d51eb5037241462786d
yamunasreeoggu <yamunasree321@gmail.com>
2023-12-14 14:39:18 -0500

    Practice | Yamuna |added silent option for downloading code

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo Delete Nginx default content
 rm -rf /usr/share/nginx/html/*
 
 echo Download and Extract code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 echo Copy Expense Conf file
 cp expense.conf /etc/nginx/default.d/expense.conf
