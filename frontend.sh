735e8b5502a5cc88feef2456686d522cc4b80e58
yamunasreeoggu <yamunasree321@gmail.com>
2023-12-14 14:32:05 -0500

    Practice | Yamuna |added echo commands

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,18 @@
+echo set hostname
+set-hostname frontend
+
+echo Install Nginx
 dnf install nginx -y
 
+echo Delete Nginx default content
 rm -rf /usr/share/nginx/html/*
 
+echo Dowmload and Extract code
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo Copy Expense Conf file
 copy expense.conf /etc/nginx/default.d/expense.conf
 
+echo restart Nginx Service
 systemctl enable nginx
 systemctl restart nginx
