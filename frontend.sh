60b8d31d730c735d196a8d9fa4d15be6f9cbe739
yamunasreeoggu <yamunasree321@gmail.com>
2023-12-14 14:16:10 -0500

    Practice | Yamuna |added frontend code

--- frontend.sh
+++ frontend.sh
@@ -1,1 +1,10 @@
-dweretrytuyiubv
+dnf install nginx -y
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+copy expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl enable nginx
+systemctl restart nginx
